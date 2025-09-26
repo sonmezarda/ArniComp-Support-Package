dfc7dc226c889fca0b76c65f052a4594eedd64a0
Arda Sönmez <ssonmezarda@gmail.com>
2025-09-27 01:01:22 +0300

    count example

--- assemblerV2_python/examples/example1.asm
+++ assemblerV2_python/examples/example1.asm
@@ -1,7 +1,7 @@
 ldi #0
 mov marl, ra
 strl ra
-ldi #3
+ldi #4
 mov marl, ra
 ldi #0
 strl ra
@@ -19,15 +19,21 @@ and ra
 ldi #2
 mov marl, ra
 strl acc
+and ra
+ldi #3
+mov marl, ra
+strl acc
 ldi #1
 mov rd, ra
+ldi #2
+mov marl, ra
 sub ml
 ldi @if_2
 mov prl, ra
 jne
 ldi #0
 mov rd, ra
-ldi #3
+ldi #4
 mov marl, ra
 sub ml
 ldi @if_1
@@ -39,20 +45,52 @@ ldrl rd
 ldi #1
 add ra
 strl acc
-ldi #3
+ldi #4
 mov marl, ra
 ldi #1
 strl ra
 if_1:
-ldi @else_1
+ldi @else_2
 mov prl, ra
 jmp
 if_2:
+ldi #2
+mov rd, ra
 ldi #3
+mov marl, ra
+sub ml
+ldi @if_4
+mov prl, ra
+jne
+ldi #0
+mov rd, ra
+ldi #4
+mov marl, ra
+sub ml
+ldi @if_3
+mov prl, ra
+jne
+ldi #0
+mov marl, ra
+ldrl rd
+ldi #1
+sub ra
+strl acc
+ldi #4
+mov marl, ra
+ldi #1
+strl ra
+if_3:
+ldi @else_1
+mov prl, ra
+jmp
+if_4:
+ldi #4
 mov marl, ra
 ldi #0
 strl ra
 else_1:
+else_2:
 ldi #0
 mov marl, ra
 ldrl rd
