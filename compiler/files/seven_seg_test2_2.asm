7f49558f99c32b53474ae6da429bd36bab69064a
sonmezarda <ssonmezarda@gmail.com>
2026-01-24 14:46:31 +0300

    Add condition test example and hex output files

--- compiler/files/seven_seg_test2_2.asm
+++ compiler/files/seven_seg_test2_2.asm
@@ -1,41 +1,44 @@
 while_start_1:
-ldi #0
-mov marl, ra
-ldi #32
-mov marh, ra
-mov rd, m
-ldi #0
-mov marh, ra
-mov m, rd
-mov rd, m
-ldi #1
-and ra
-inx
-mov m, acc
-ldi #1
-mov rd, ra
-inx
-ldi #0
-mov marh, ra
-cmp m
-ldi @if_1
-mov prl, ra
-jne
-ldi #9
-mov rd, ra
-ldi #0
-mov marl, ra
-ldi #16
-mov marh, ra
-mov m, rd
-ldi @else_1
-mov prl, ra
-jmp
-if_1:
-ldi #2
-mov rd, ra
-mov m, rd
-else_1:
-ldi @while_start_1
-mov prl, ra
-jmp
+    ldi #0
+    mov marl, ra
+    ldi #32
+    mov marh, ra ; mar = 0x2000
+    ;mov m, rd
+
+    ldi #0
+    mov marh, ra ; mar = 0x0000
+
+    mov m, rd
+    mov rd, m
+    ldi #1
+    and ra
+    inx ; mar = 0x0001
+    mov m, acc
+    ldi #1
+    mov rd, ra
+    inx ; mar = 0x0002
+    mov m, acc
+    ldi #0
+    mov marh, ra ; mar = 0x0002
+    cmp m 
+    ldi @if_1
+    mov prl, ra
+    jne
+    ldi #9
+    mov rd, ra
+    ldi #0
+    mov marl, ra
+    ldi #16
+    mov marh, ra
+    mov m, rd
+    ldi @else_1
+    mov prl, ra
+    jmp
+    if_1:
+    ldi #2
+    mov rd, ra
+    mov m, rd
+    else_1:
+    ldi @while_start_1
+    mov prl, ra
+    jmp
