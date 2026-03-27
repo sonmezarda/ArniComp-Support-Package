85c28ca5c69554ba5b499b6a5ef24ab8bc2d7be1
Arda Sönmez <ssonmezarda@gmail.com>
2026-03-27 18:56:34 +0300

    uart tx test with polling fpga

--- assembler/examples/fpga_uart_tx.asm
+++ assembler/examples/fpga_uart_tx.asm
@@ -2,6 +2,7 @@ equ UART_BASE_H 0x09
 equ UART_CNTRL_L 0x40 ; {5'b0, TX_EN, RX_EN, UART_EN}
 equ UART_BAUDSEL_L 0x20
 equ UART_TX_DATA_L 0x10
+equ UART_TX_READY_L 0x11
 
 equ SEND_CHAR 65;
 
@@ -24,7 +25,7 @@ mov m, ra ; enable tx and uart
 ldi $UART_BAUDSEL_L
 mov marl, ra
 
-ldi #2
+ldi #7
 mov m, ra ; baud_rate = 9600
 
 ldi $UART_TX_DATA_L
@@ -32,10 +33,25 @@ mov marl, ra
 ldi $SEND_CHAR
 mov rb, ra
 
+ldi #1
+mov rd, ra
 
-mov m, rb
-mov m, rb
-mov m, rb
+
+ldi @wait_until_ready
+mov prl, ra
+
+wait_until_ready:
+    
+    ldi $UART_TX_READY_L
+    mov marl, ra
+    mov ra, m ; ra = TX_READY
+    cmp ra    ; rd cmp rd (rd=1)
+    jne       ; jmp if ra != 1
+
+    ldi $UART_TX_DATA_L
+    mov marl, ra
+    mov m, rb
+    jmp
 
 HLT
 HLT
